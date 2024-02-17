14dbd79291567c5679edbd6115b8eed29f3694b4
reasonablep <140934359+reasonablep@users.noreply.github.com>
2024-02-16 21:23:18 -0800

    Pushing up

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,19 @@
 require("dotenv").config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+
+mongoose.connect(mongoURI, { 
+    useNewUrlParser: true, // Use new URL parser
+    useUnifiedTopology: true // Use new server discovery and monitoring engine
+})
+.then(() => {
+    console.log('MongoDB connected successfully');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+});
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
