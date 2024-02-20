49a7dc34bb4a67c4554e3337ceed01859240da96
reasonablep <140934359+reasonablep@users.noreply.github.com>
2024-02-20 01:05:12 -0800

    Added link to readme, removed if statement on server.js file

--- server/server.js
+++ server/server.js
@@ -29,8 +29,9 @@ const StartApolloServer = async () => {
 
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-}
+// if (process.env.NODE_ENV === 'production') {
+
+// }
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
